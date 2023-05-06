238bbc348df661a8ffc60f7c2d76803180964511
Danny B <danny.beaudoin@student.sl.on.ca>
2023-05-06 13:01:12 -0400

    Broken delete functionality

--- src/Components/PostingDetails.js
+++ src/Components/PostingDetails.js
@@ -1,21 +1,84 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import editIcon from '../Assets/edit.png'
+import deleteIcon from '../Assets/delete.png'
+import deletePosting from '../Services/PostingService';
+import { useQuery } from '@tanstack/react-query';
 
+
+let tempData;
 export default function PostingDetails(props) {
     const location = useLocation();
     const data = location.state?.todo;
-    console.log(data)
+
+    tempData = data;
+    // const deletePost = (data) => () => {
+    //     console.log(data)
+    //     // console.log('clicked delete', data)
+    //     // // console.log(data)
+
+    //     const {
+    //         data: test
+    //     } = useQuery({
+    //         queryKey: ["tempData"],
+    //         queryFn: deletePosting
+    //     });
+    //     deletePosting(data);
+    // }
+
+    // const HandleClick = () => {
+    //     console.log('refetching')
+    //     // manually refetch
+    //     const { newData } = useQuery(["posting", data.id], () => deletePosting(data.id))
+    // };
+
+    // const { refetch } = useQuery({ 
+    //     queryKey : ["data"], 
+    //     queryFn: deletePosting, 
+        
+    //     refetchOnWindowFocus: false,
+    //     enabled: false // disable this query from automatically running
+    // });
+
+    
+
     return (
-        <div className='postingDetails'>
-            <h1>{data.title}</h1>
-            <hr/>
-            <p>Company: {data.company}</p>
-            <p>Job posting: <a href={data.url}>{data.url}</a></p>
-            <p>Description: {data.desc}</p>
-            <p>Application Date: {data.dateApplied}</p>
-            <p>Platform Applied on: {data.platform}</p>
-            <p>Number of Interviews: {data.interviewNo}</p>
-            <p>Technical Assignment: {data.isHadTask.toString()}</p>
-        </div>
+        <>
+            <div className='postingDetails'>
+                <div>
+                    <h1>{data.title}</h1>
+                    <hr />
+                </div>
+                <div className="button-panel">
+                    <Link to='/viewPostings'>
+                        <button className='btn btn-secondary'>Back</button>
+                    </Link>
+                    <button data={data} onClick={editPosting}><img src={editIcon} alt='edit' className='button-img' /></button>
+                    <button onClick={deletePosting(data.id)}><img src={deleteIcon} alt='delete' className='button-img' /></button>
+                </div>
+                <div>
+                    <p>Company: {data.company}</p>
+                    <p>Job posting: <a href={data.url}>{data.url}</a></p>
+                    <p>Description: {data.desc}</p>
+                    <p>Application Date: {data.dateApplied}</p>
+                    <p>Platform Applied on: {data.platform}</p>
+                    <p>Number of Interviews: {data.interviewNo}</p>
+                    <p>Technical Assignment: {data.isHadTask.toString()}</p>
+                </div>
+            </div>
+        </>
     )
+}
+
+function DeleteThisPosting() {
+    const {
+        data: test
+    } = useQuery({
+        queryKey: ["tempData"],
+        queryFn: deletePosting
+    });
+}
+
+function editPosting() {
+    console.log('edit clicked')
 }
--- src/Services/PostingService.js
+++ src/Services/PostingService.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
-export default function fetchPostings() {
-    let baseUri = "http://192.168.0.102:8888/api/postings";
+let baseUri = "http://192.168.0.102:8888/api/postings";
+
+export default function fetchPostings() {    
     const data = axios.get(baseUri).then((res) => res.data);
     return data;
+}
+
+export function deletePosting(id){
+    axios.delete(baseUri + '/${id}')
+    console.log(baseUri, '/1')
+    //const res = axios.delete(baseUri + '/${id}').then((res) => res.data);
 }
